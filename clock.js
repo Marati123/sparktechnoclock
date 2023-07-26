1410dd7d099366c43c5abe859cbd4228b9b2ba4c
MARATI PAVAN <pavanmarati068@gmail.com>
2023-07-26 12:45:50 +0530

    first commit

---
+++ clock.js
@@ -0,0 +1,37 @@
+function displayTime() {
+    var date = new Date();
+    var hour = date.getHours();
+    var min = date.getMinutes();
+    var sec = date.getSeconds();
+
+
+    let amorpm = "am";
+    
+
+
+    if (hour >= 12) {
+        amorpm = "pm";
+    }
+
+    if (hour > 12) {
+        hour = hour - 12;
+    }
+    if (hour < 10) {
+        hour = "0" + hour;
+    }
+    if (min < 10) {
+        min = "0" + min;
+    }
+    if (sec < 10) {
+        sec = "0" + sec;
+    }
+
+
+
+    document.getElementById("clock").innerHTML = hour + ":" + min + ":" + sec + " " + amorpm;
+
+
+
+
+}
+setInterval(displayTime, 1000)
